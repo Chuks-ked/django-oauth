c0ea24f48e8d7a7eb7420b0a076171ac21243bfd
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-09-14 00:07:07 +0100

    custom admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import CustomUser
 # Register your models here.
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('id', 'email', 'is_staff', 'is_superuser', 'is_active', 'date_joined', 'auth_provider',)
+    list_filter = ('is_staff', 'is_rider', 'is_active', 'is_superuser')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name', 'phone_number', 'location', 'profile_picture',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_rider', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
