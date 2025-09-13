a0abc95a24bf506c3f9cd10da21d0de2da1c62cb
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-09-14 00:06:11 +0100

    email/password auth added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 # accounts/urls.py
 from django.urls import path
-from .views import GoogleSocialAuthView
+from .views import *
 from rest_framework_simplejwt.views import TokenRefreshView  # For refresh endpoint
 
 urlpatterns = [
     path('auth/google/', GoogleSocialAuthView.as_view(), name='google_auth'),
-    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Mobile can POST {"refresh": "..."} here
+    path('auth/login/', EmailTokenObtainPairView.as_view(), name='login'),
+    path('auth/signup/', RegisterView.as_view(), name='signup'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 # In project/urls.py: path('api/', include('accounts.urls')),
